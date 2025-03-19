c64e5652e2f7b671b893a4496f06315a8d856c95
unknown <inyastephanas@gmail.com>
2025-03-19 12:42:53 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,19 @@
+let side1 = 387
+
+let side2 = 38
+
+let side3 = 388
+
+if(side1 === side2 && side2 === side3)(
+    console.log("equilateral triangle")
+)
+
+
+if(side1 === side2 || side1 === side3 || side2 === side3)(
+    console.log("isosceles triangle")
+)
+
+else(
+    console.log("scalene triangle")
+)
+
