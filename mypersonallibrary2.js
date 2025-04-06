b946ced61c85a8ca66ba308925276d1e84273bd6
unknown <inyastephanas@gmail.com>
2025-04-06 19:11:17 +0100

    correctio for assighnment 2&5

--- mypersonallibrary2.js
+++ mypersonallibrary2.js
@@ -3,8 +3,12 @@ const book = {
     description: "Quest for Justice and Truth", 
     numberOfPages: 264,
     author: "William shakespare",
-    reading: false,
-};
+    reading: true,
 
-console.log(book.reading)
+toggleReadingStatus: function(){
+    book.reading = !book.reading;
+    console.log(`is the book ${book.title} currently been read? ${book.reading}.`)
+}
+}
+book.toggleReadingStatus()
 
--- returnfunction.js
+++ returnfunction.js
@@ -1,6 +1,5 @@
 function calculateAreaOfRectangle(length, width){
-    return (length * width) / 2;
+    return (length * width)
 }
-let length = 20;
-let width = 10;
-console.log(calculateAreaOfRectangle.return)
+let area = (3 * 2)
+console.log(area)
